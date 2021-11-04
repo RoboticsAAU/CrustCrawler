0836acb954d6dce53d4ae37da9bba7e996b4e882
Alexander Philip Ettrup Riedel <ariede20@student.aau.dk>
2021-11-04 10:47:38 +0100

    Initial Commit

---
+++ Arduino/Arduino.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Arduino.ino
+ Created:	11/4/2021 10:46:57 AM
+ Author:	alexa
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
