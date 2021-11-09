8a83da4f365bf109c9f228143488a6cc133611f3
ToRexZ <victor.risager@hotmail.com>
2021-11-09 08:54:20 +0100

    removed this keywords.

--- Arduino/ComputerConnector.cpp
+++ Arduino/ComputerConnector.cpp
@@ -15,20 +15,20 @@ ComputerConnector::~ComputerConnector() {
 void ComputerConnector::updateComputerData() {
 	if (DATA_SERIAL.available()) {
 
-		this->_newData = DATA_SERIAL.readStringUntil('\n');
-		if (this->_newData == this->_currentData && this->_newData != "00000") {
+		_newData = DATA_SERIAL.readStringUntil('\n');
+		if (_newData == _currentData && _newData != "00000") {
 			return;
 		}
-		this->_currentData = this->_newData;
+		_currentData = _newData;
 
 		String tmpSpeedString = _newData.substring(3);
 		int arraySize = _newData.length();
 
 		
-		this->emergencyStop = this->_currentData[0] - '0';
-		this->controlMode = this->_currentData[1] - '0';
-		this->direction = this->_currentData[2] - '0';
-		this->speed = tmpSpeedString.toDouble();
+		emergencyStop = _currentData[0] - '0';
+		controlMode = _currentData[1] - '0';
+		direction = _currentData[2] - '0';
+		speed = tmpSpeedString.toDouble();
 
 	}
 
