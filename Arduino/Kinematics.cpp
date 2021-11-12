9dbcde10e4f2a73c6b1c53a0d176b857f0dc9563
Magnus Kjaedegaard <magnus@kjaedegaard.dk>
2021-11-12 09:18:12 +0100

    Changed AngleData.m_thetaX to AngleData.currentThetas[X] in Kinematics.cpp

--- Arduino/Kinematics.cpp
+++ Arduino/Kinematics.cpp
@@ -6,7 +6,7 @@ Kinematics::Kinematics() {}
 Kinematics::~Kinematics() {}
 
 void Kinematics::UpdateForwardkinematics() {
-	eePositionData.x = -cos(AngleData.m_Theta1) * (Joint2.m_length * sin(AngleData.m_Theta2) + Joint3.m_length * sin(AngleData.m_Theta2 + AngleData.m_Theta3));
-	eePositionData.y = -sin(AngleData.m_Theta1) * (Joint2.m_length * sin(AngleData.m_Theta2) + Joint3.m_length * sin(AngleData.m_Theta2 + AngleData.m_Theta3));
-	eePositionData.z = Joint1.m_length + Joint2.m_length * cos(AngleData.m_Theta2) + Joint3.m_length * cos(AngleData.m_Theta2 + AngleData.m_Theta3);
+	eePositionData.x = -cos(AngleData.m_currentThetas[0]) * (Joint2.m_length * sin(AngleData.m_currentThetas[1]) + Joint3.m_length * sin(AngleData.m_currentThetas[1] + AngleData.m_currentThetas[2]));
+	eePositionData.y = -sin(AngleData.m_currentThetas[0]) * (Joint2.m_length * sin(AngleData.m_currentThetas[1]) + Joint3.m_length * sin(AngleData.m_currentThetas[1] + AngleData.m_currentThetas[2]));
+	eePositionData.z = Joint1.m_length + Joint2.m_length * cos(AngleData.m_currentThetas[1]) + Joint3.m_length * cos(AngleData.m_currentThetas[1] + AngleData.m_currentThetas[2]);
 }
