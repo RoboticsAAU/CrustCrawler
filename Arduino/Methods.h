54fedf0e11db667a80c6ec93695395cf59c2a5c3
Daniel D <73134670+DaniDoro@users.noreply.github.com>
2022-01-12 13:33:01 +0100

    Trivial delete

--- Arduino/Methods.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "CrustCrawlerData.h"
-
-#include <BasicLinearAlgebra.h>
-#include <math.h>
-
-extern double IntegrationOperator(double currentValue, double inputIntegrationVal, unsigned long& looptime);
-extern double DifferentiationOperator(double currentValue, double previousValue, unsigned long& looptime);
-extern void SpaceConverter(SpaceType desiredSpace);
